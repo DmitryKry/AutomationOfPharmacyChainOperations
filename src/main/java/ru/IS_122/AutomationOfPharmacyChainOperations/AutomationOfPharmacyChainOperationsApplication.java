dd0c5719ac8e448e600aabc94010a9d4a5690eb3
DmitryKry <dkyulov@mail.ru>
2026-04-08 13:30:17 +0300

    first commit

---
+++ src/main/java/ru/IS_122/AutomationOfPharmacyChainOperations/AutomationOfPharmacyChainOperationsApplication.java
@@ -0,0 +1,13 @@
+package ru.IS_122.AutomationOfPharmacyChainOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationOfPharmacyChainOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationOfPharmacyChainOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/IS_122/AutomationOfPharmacyChainOperations/AutomationOfPharmacyChainOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.IS_122.AutomationOfPharmacyChainOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationOfPharmacyChainOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
